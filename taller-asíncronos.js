da41802400c836df0f5cb9be06d3398d8c2d9bff
espiritudeaire <espiritudeaire@gmail.com>
2024-10-26 00:57:01 -0500

    actualizando

--- taller-asíncronos.js
+++ taller-asíncronos.js
@@ -54,3 +54,55 @@ sumarAsync(100, 5)
     .catch(e => {
         console.log('Error:', e);
     });
+
+function cargarUsuario() {
+    return new Promise(resolve => setTimeout(() => resolve('Usuarios Cargados'), 1000))
+}
+
+function cargarProductos() {
+    return new Promise(resolve => setTimeout(() => resolve('Productos Cargados'), 2000))
+}
+
+function cargarPedidos() {
+    return new Promise((_, reject) => setTimeout(() => reject('Pedidos Cargados'), 3000))
+}
+
+async function cargarTodos() {
+    try {
+        const resultados = await Promise.allSettled([cargarUsuario(), cargarPedidos(), cargarProductos()])
+        console.log(resultados);
+
+    } catch (error) {
+        console.log('ERROR:', error);
+
+    }
+}
+
+cargarTodos()
+
+
+function tarea(numero) {
+    return new Promise(resolve => setTimeout(() => {
+        console.log(`Tarea ${numero} completada`);
+        resolve(`Tarea ${numero}`);
+    }, 1000));
+}
+
+async function ejecutarConLimite(tareas, limite) {
+    const cola = [...tareas];  // Clonamos el array de tareas
+    const resultados = [];     // Aquí se almacenarán los resultados
+
+    while (cola.length) {
+        console.log(`Tareas restantes: ${cola.length}`);
+        const ejecuciones = cola.splice(0, limite).map(t => t());
+        console.log(`Ejecutando lote de ${ejecuciones.length} tareas`);
+
+        const resultadoGrupo = await Promise.all(ejecuciones);
+        resultados.push(...resultadoGrupo); // Añade resultados al array principal
+        console.log(`Resultados hasta ahora: ${resultados}`);
+    }
+
+    console.log('Todas las tareas completadas:', resultados);
+}
+
+ejecutarConLimite([() => tarea(1), () => tarea(2), () => tarea(3), () => tarea(4)], 2);
