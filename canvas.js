f5b04c716292d0507bde5f8041e1189ea83a28ce
espiritudeaire <espiritudeaire@gmail.com>
2024-11-06 15:26:12 -0500

    act

--- canvas.js
+++ canvas.js
@@ -331,11 +331,36 @@ function gradientes() {
     context.fillRect(10, 10, 200, 100);
 }
 
-let img = new Image();
-img.src = 'img.png';
-img.onload = () => {
-    // create pattern
-    const pattern = context.createPattern(img, "repeat");
-    context.fillStyle = pattern;
-    context.fillRect(0, 0, 100, 250);
-};
+function patrones() {
+    let img = new Image();
+    img.src = 'img.png';
+    img.onload = () => {
+        // create pattern
+        const pattern = context.createPattern(img, "repeat");
+        context.fillStyle = pattern;
+        context.fillRect(0, 0, 100, 250);
+    };
+}
+
+function draw2() {
+    const ctx = document.getElementById("canvas").getContext("2d");
+
+    ctx.fillRect(0, 0, 150, 150); // Draw a Black rectangle with default settings
+    ctx.save(); // Save the original default state
+
+    ctx.fillStyle = "#09F"; // Make changes to saved settings
+    ctx.fillRect(15, 15, 120, 120); // Draw a Blue rectangle with new settings
+    ctx.save(); // Save the current state
+
+    ctx.fillStyle = "#FFF"; // Make changes to saved settings
+    ctx.globalAlpha = 0.5;
+    ctx.fillRect(30, 30, 90, 90); // Draw a 50%-White rectangle with newest settings
+
+    ctx.restore(); // Restore to previous state
+    ctx.fillRect(45, 45, 60, 60); // Draw a rectangle with restored Blue setting
+
+    ctx.restore(); // Restore to original state
+    ctx.fillRect(60, 60, 30, 30); // Draw a rectangle with restored Black setting
+}
+
+draw2();
